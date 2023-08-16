3455066c8185d06285f26d4f33554490f6b56e31
JuanBogliacino <juanboglia12@gmail.com>
2023-08-16 19:14:14 -0300

    validations form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,30 +1,35 @@
 import { useState } from "react";
 
 const Contact = () => {
-  const [error, setError] = useState(null)
+  const [errors, setErrors] = useState({})
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleSubmit = (e) => {
-    const name = e.target.name.value.trim()
-    const email = e.target.email.value.trim()
-    const message = e.target.message.value.trim()
-  
-    if (name === '') {
-      e.preventDefault()
-      return setError('Fill in the Name field')
+    // Validaciones
+    const newErrors = {};
+
+    if (name.trim() == '') {
+      newErrors.name = 'Name is required';
     }
-  
-    if (email === '') {
-      e.preventDefault()
-      return setError('Fill in the Email field')
+
+    if (email.trim() == '') {
+      newErrors.email = 'Email is required';
     }
-  
-    if (message === '') {
-      e.preventDefault()
-      return setError('Fill in the Message field')
+
+    if (message.trim() == '') {
+      newErrors.message = 'Message is requerid'
     }
-  
-    return
-  }  
+
+    // Si hay errores, no enviar el formulario
+    if (Object.keys(newErrors).length > 0) {
+      e.preventDefault();
+      setErrors(newErrors);
+    } else {
+      return
+    }
+  };
 
   return (
     <div name='contact' className='w-full flex justify-center items-center p-4 pt-[150px]'>
@@ -33,13 +38,17 @@ const Contact = () => {
                 <p className='text-4xl font-bold inline border-b-4 border-green-600 text-gray-300 font-tektur'>Contact</p>
                 <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - bogliacinojuan0@gmail.com</p>
             </div>
-            <input className='bg-[#ccd6f6] p-2 outline-none focus:outline-green-600' type="text" placeholder='Name' name='name' />
-            <input className='my-4 p-2 bg-[#ccd6f6] outline-none focus:outline-green-600' type="email" placeholder='Email' name='email' />
-            <textarea className='bg-[#ccd6f6] p-2 outline-none focus:outline-green-600' name="message" rows="10" placeholder='Message'></textarea>
+
+            <input className='bg-[#ccd6f6] p-2 outline-none focus:outline-green-600' type="text" value={name} placeholder='Name' name='name' onChange={(e) => setName(e.target.value)} />
+            {errors.name && <p className='text-red-600 mt-2'>{errors.name}</p>}
+
+            <input className='mt-4 p-2 bg-[#ccd6f6] outline-none focus:outline-green-600' type="email" value={email} placeholder='Email' name='email' onChange={(e) => setEmail(e.target.value)} />
+            {errors.email && <p className='text-red-600 mt-2'>{errors.email}</p>}
+
+            <textarea className='mt-4 bg-[#ccd6f6] p-2 outline-none focus:outline-green-600' value={message} name="message" rows="10" placeholder='Message' onChange={(e) => setMessage(e.target.value)}></textarea>
+            {errors.message && <p className='text-red-600 mt-2'>{errors.message}</p>}
+
             <button className='text-white border-2 hover:bg-green-600 hover:border-green-600 px-4 py-3 my-8 mx-auto flex items-center'>Send Email</button>
-            {
-              error && <p className="text-[#f00] text-center mb-3 text-[18px]">{error}</p>
-            }
         </form>
     </div>
   )
